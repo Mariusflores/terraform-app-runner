a697c8dc345e07d27b6017f1cf9946d8e942794e
mariusflores <mfloresdal@gmail.com>
2023-10-10 10:00:40 +0000

    commit

--- provider.tf
+++ provider.tf
@@ -5,10 +5,9 @@ terraform {
       version = "4.39.0"
     }
   }
-  backend "s3" {
+ backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "glenn.richard.bech/apprunner-a-new-state.state"
+    key    = "mada060/apprunner-actions.state"
     region = "eu-north-1"
-  }
+ }
 }
-
